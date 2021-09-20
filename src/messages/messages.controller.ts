bc376fec2e290d373d3e1f519d0b3674465a8694
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-09-20 23:05:56 +0100

    wiring up service to controller, app ready

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {}
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {}
+  getMessage(@Param('id') id: string) {
+    this.messagesService.findOne(id);
+  }
 }
